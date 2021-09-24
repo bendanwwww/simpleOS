23bad3578d45aa07d2065f97c9e55453b7baf030
bendanwwww <461806307@qq.com>
2021-09-24 11:04:00 +0800

    add

---
+++ shell/xxd.sh
@@ -0,0 +1,20 @@
+#usage: sh xxd.sh 文件起始地址长度
+xxd -u -a -g 1 -s $2 -l $3 $1
+# -u 
+# use upper case hex letters. Default is lower case.
+#
+# -a | -autoskIP
+# toggle autoskIP: A single '*' replaces nul-lines. Default off.
+#
+# -g bytes | -groupsize bytes
+# separate the output of every <bytes> bytes (two hex characters or eight bit-digits each) by a whitespace. Specify -g 0 to
+# suppress grouping. <Bytes> defaults to 2 in normal mode and 1 in bits mode. Grouping does not apply to postscrIPt or
+# include style.
+#
+# -c cols | -cols cols
+# format <cols> octets per line. Default 16 (-i: 12, -ps: 30, -b: 6). Max 256.
+#
+# -s [+][-]seek
+# start at <seek> bytes abs. (or rel.) infile offset. + indicates that the seek is relative to the current stdin file position
+# (meaningless when not reading from stdin). - indicates that the seek should be that many characters from the end of
+# the input (or if combined with +: before the current stdin file position). Without -s option, xxd starts at the current file position.
